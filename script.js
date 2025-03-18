e1d8c6f25f1224093c9693908ed241c602c58508
kartik120-pk <118261121+kartik120-pk@users.noreply.github.com>
2025-03-18 20:08:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-class car{
-    constructor(make, model){
-        this.make = Toyota;
-        this.model = Corolla;
+
+class Car {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
     }
-    getMakeModel(){
+
+    getMakeModel() {
         return this.make + " " + this.model;
     }
 }
 
-class SportsCar extends car{
-    constructor(make, model, topSpeed){
-        super(make, model);
-        this.topSpeed = 400;
+
+class SportsCar extends Car {
+    constructor(make, model, topSpeed) {
+        super(make, model); 
+        this.topSpeed = topSpeed;
     }
-    getTopSpeed(){
+
+    getTopSpeed() {
         return this.topSpeed;
     }
 }
 
-let car = new SportsCar(make, model, topSpeed);
-console.log(car.getMakeModel());
-console.log(car.getTopSpeed());
-// Do not change the code below
+
+let car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); 
+console.log(car.getTopSpeed());  
+
+
 window.Car = Car;
 window.SportsCar = SportsCar;
